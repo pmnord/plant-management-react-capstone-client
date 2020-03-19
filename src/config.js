ac2c28e0c6c1b472f132784b4526a655068b9e14
Peter Nordman <petermnordman@gmail.com>
2020-03-19 11:19:55 -0400

    config API endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export default {
-    API_ENDPOINT: process.env.NODE_ENV === "development"
-        ? "http://localhost:8000/api"
-        : process.env.REACT_APP_FANCYPLANTS_SERVER,
+    API_ENDPOINT: process.env.REACT_APP_FANCYPLANTS_SERVER,
     API_KEY: process.env.REACT_APP_FANCYPLANTS_API_KEY,
     TOKEN_KEY: `Authorization`,
 }
