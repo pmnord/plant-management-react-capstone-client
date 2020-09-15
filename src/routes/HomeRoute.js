e8e6dd83440d53a92b72964c74fa9431a4874329
Peter Nordman <peter.m.nordman@gmail.com>
2020-09-15 12:44:56 -0400

    restyle the landing page to remove lines, improve buttons, and lighten boxes

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -7,8 +7,8 @@ export default function HomeRoute(props) {
     return (
         <div className="HomeRoute">
             <div className="HomeRoute__pitch">
-                <p><strong><u>To demo the app use the following credentials</u></strong></p>
-                <p><strong>Username: demo | Password: password</strong></p>
+                {/* <p><strong><u>To demo the app use the following credentials</u></strong></p>
+                <p><strong>Username: demo | Password: password</strong></p> */}
                 <p>Fancy Plants allows you to create a virtual garden for all of your plants, get in-depth information on plant care, and share your personal garden with other house plant enthusiasts.
                         <br />
                         <br />
