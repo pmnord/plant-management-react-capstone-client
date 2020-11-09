62799a6a3daaf534f3c4a9c2d96db8fb0195cd22
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-09 14:19:00 -0500

    Rename Toolbar component to TabBar. Rename Garden and Plant Explorer tabs in TabBar to Collection and Plant Search. Plan to rename all instances of Garden to Collection throughout the app.

--- src/components/TabBar/TabBar.test.js
+++ src/components/TabBar/TabBar.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Toolbar from './Toolbar';
+import TabBar from './TabBar';
 
-it('Renders the Toolbar component', () => {
+it('Renders the TabBar component', () => {
     const div = document.createElement('div');
 
-    ReactDOM.render(<Toolbar />, div);
+    ReactDOM.render(<TabBar />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
